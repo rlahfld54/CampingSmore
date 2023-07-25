7f081a5c30ed6e56fa3ebe1a45259655f590bd6d
장하은 <haun0@naver.com>
2023-07-25 17:32:03 +0900

    database

---
+++ src/main/java/com/green/campingsmore/item/model/ItemInsParam.java
@@ -0,0 +1,12 @@
+package com.green.campingsmore.item.model;
+
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+public class ItemInsParam {
+    private String name;
+    private int price;
+    private String pic;
+}
