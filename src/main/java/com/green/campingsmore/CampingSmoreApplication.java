0ae9c22aaf19574df0d7b65dea18caf0533c875f
황주은 <rlahfld54@naver.com>
2023-07-25 14:47:11 +0900

    first commit

---
+++ src/main/java/com/green/campingsmore/CampingSmoreApplication.java
@@ -0,0 +1,13 @@
+package com.green.campingsmore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampingSmoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampingSmoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/green/campingsmore/CampingSmoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.campingsmore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampingSmoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
