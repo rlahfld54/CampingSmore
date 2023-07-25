47a4c0d2c16172db20b85ed7a673db254c143c3a
장하은 <haun0@naver.com>
2023-07-25 19:39:36 +0900

    item - haeun

--- src/main/java/com/green/campingsmore/item/ItemMapper.java
+++ src/main/java/com/green/campingsmore/item/ItemMapper.java
@@ -1,7 +1,11 @@
 package com.green.campingsmore.item;
 
+import com.green.campingsmore.item.model.ItemInsDto;
+import com.green.campingsmore.item.model.ItemInsParam;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface ItemMapper {
+    int insItem(ItemInsParam p);
+    int selCate(ItemInsDto cate);
 }
--- src/main/java/com/green/campingsmore/item/ItemService.java
+++ src/main/java/com/green/campingsmore/item/ItemService.java
@@ -1,10 +1,15 @@
 package com.green.campingsmore.item;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.green.campingsmore.dataset.NaverApi;
+import com.green.campingsmore.item.model.ItemInsDto;
+import com.green.campingsmore.item.model.ItemInsParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -15,17 +20,36 @@ public class ItemService {
 
     public String insItem(String text) {
 
-/*
         String json = naverApi.search(text);
+
+        System.out.println("json!!!!");
         ObjectMapper mapper = new ObjectMapper();
-        Map<String, Object> map = mapper.readValue(json, Map.class);
-        // 여기서 캐스팅 안되는듯..
-        List<LinkedHashMap<String, Object>> numList = (List<LinkedHashMap<String, Object>>) map.get("item");
+        Map<String, Object> map = null;
+        ItemInsParam ipram = new ItemInsParam();
+        ItemInsDto dto = new ItemInsDto();
+        try {
+            map = mapper.readValue(json, Map.class);
+            List<LinkedHashMap<String, Object>> numList = (List<LinkedHashMap<String, Object>>) map.get("items");
+            System.out.println(numList);
+            for(LinkedHashMap<String, Object> item : numList) {
 
-*/
+
+                dto.setCategory((String)item.get("category2"));
+                dto.setName((String)item.get("title"));
+                dto.setPrice((Integer)item.get("lprice"));
+                dto.setPic((String)item.get("image"));
 
 
-        return null;
+            }
+
+
+
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        return json;
     }
 
 }
