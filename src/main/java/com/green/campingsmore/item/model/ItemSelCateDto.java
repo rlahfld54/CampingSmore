5d1849ca7fb1dbafca917793ac0d5545422cf583
장하은 <haun0@naver.com>
2023-07-27 18:05:21 +0900

    selCateItem - haeun

--- src/main/java/com/green/campingsmore/item/model/ItemSelCateDto.java
+++ src/main/java/com/green/campingsmore/item/model/ItemSelCateDto.java
@@ -3,7 +3,7 @@ package com.green.campingsmore.item.model;
 import lombok.Data;
 
 @Data
-public class ItemSelPageDto {
+public class ItemSelCateDto {
     private Long iitemCategory;
     private int startIdx;
     private int page;
