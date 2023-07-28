4006555ff0c2067f8476d54d2a8411826bdd9abe
장하은 <haun0@naver.com>
2023-07-28 10:49:27 +0900

    selCateItem - haeun

--- src/main/java/com/green/campingsmore/sign/SignController.java
+++ src/main/java/com/green/campingsmore/sign/SignController.java
@@ -25,7 +25,7 @@ public class SignController {
     //RequestParam은 http 로부터 요청 온 정보를 받아오기 위한 스프링 어노테이션이다.
     @PostMapping("/sign-in")
     @Operation(summary = "로그인",
-            description = "Try it out -> Execute 눌러주세요 \n\n "+
+            description = "Try it out -> Execute 눌러주세요 \n\n " +
                     "id:  아이디 \n\n " +
                     "password : 비밀번호 \n\n "
     )
@@ -35,7 +35,7 @@ public class SignController {
         log.info("[signIn] 로그인을 시도하고 있습니다. id: {}, pw: {}, ip: {}", id, password, ip);
 
         SignInResultDto dto = SERVICE.signIn(id, password, ip);
-        if(dto.getCode() == CommonRes.SUCCESS.getCode()) {
+        if (dto.getCode() == CommonRes.SUCCESS.getCode()) {
             log.info("[signIn] 정상적으로 로그인 되었습니다. id: {}, token: {}", id, dto.getAccessToken());
         }
         return dto;
@@ -43,7 +43,7 @@ public class SignController {
 
     @PostMapping("/sign-up")
     @Operation(summary = "회원가입",
-            description = "Try it out -> Execute 눌러주세요 \n\n "+
+            description = "Try it out -> Execute 눌러주세요 \n\n " +
                     "uid: 회원가입 아이디 \n\n " +
                     "upw : 비밀번호 ex)1234 \n\n " +
                     "email : 이메일 ex)rlahfld54@gmail.com \n\n " +
@@ -54,7 +54,7 @@ public class SignController {
                     "role: USER 이거나 ADMIN\n\n "
     )
     public SignUpResultDto signUp(@RequestBody SignUpDto signUpDto) {
-        log.info("[signUp] 회원가입을 수행합니다. id: {}, pw: {}, nm: {}, role: {}", signUpDto.getUid(), signUpDto.getUpw(),signUpDto.getName(), signUpDto.getRole());
+        log.info("[signUp] 회원가입을 수행합니다. id: {}, pw: {}, nm: {}, role: {}", signUpDto.getUid(), signUpDto.getUpw(), signUpDto.getName(), signUpDto.getRole());
         SignUpResultDto dto = SERVICE.signUp(signUpDto);
         log.info("[signUp] 회원가입 완료 id: {}", signUpDto.getUid());
         return dto;
@@ -62,7 +62,7 @@ public class SignController {
 
     @GetMapping("/refresh-token")
     @Operation(summary = "리프레쉬 토큰",
-            description = "Try it out -> Execute 눌러주세요 \n\n "+
+            description = "Try it out -> Execute 눌러주세요 \n\n " +
                     "refreshToken :  리프레쉬 토큰 \n\n "
     )
     public ResponseEntity<SignUpResultDto> refreshToken(HttpServletRequest req, @RequestParam String refreshToken) {
@@ -72,37 +72,29 @@ public class SignController {
 
     @GetMapping("/search-id")
     @Operation(summary = "아이디 찾기",
-            description = "Try it out -> Execute 눌러주세요 \n\n "+
+            description = "Try it out -> Execute 눌러주세요 \n\n " +
                     "user_id :  아이디 찾기 \n\n "
     )
     public String searchID(@RequestParam String name
-                         ,@RequestParam String phone
-                         ,@RequestParam String birth
-    ){
-        return SERVICE.searchID(name,phone,birth);
+            , @RequestParam String phone
+            , @RequestParam String birth
+    ) {
+        return SERVICE.searchID(name, phone, birth);
     }
 
     @DeleteMapping("/delete-user")
     @Operation(summary = "회원탈퇴",
-            description = "Try it out -> Execute 눌러주세요 \n\n "+
+            description = "Try it out -> Execute 눌러주세요 \n\n " +
                     "iuser:  iuser PK \n\n "
     )
-    public void deleteUser(@RequestParam int iuser){
+    public void deleteUser(@RequestParam int iuser) {
         SERVICE.deleteUser(iuser);
     }
 
 
-    @PostMapping("/sign-up")
+    @PostMapping("/updateUserInfo")
     @Operation(summary = "회원 정보 수정",
-            description = "Try it out -> Execute 눌러주세요 \n\n "+
-                    "uid: 회원가입 아이디 \n\n " +
-                    "upw : 비밀번호 ex)1234 \n\n " +
-                    "email : 이메일 ex)rlahfld54@gmail.com \n\n " +
-                    "name: 이름 \n\n " +
-                    "birth_date: 생년월일 ex)1998-06-12 \n\n " +
-                    "phone: 핸드폰 번호 \n\n " +
-                    "user_address: 주소 \n\n " +
-                    "role: USER 이거나 ADMIN\n\n "
+            description = "Try it out -> Execute 눌러주세요 \n\n "
     )
     public void updateUserInfo(@RequestBody SignUpDto signUpDto) {
         SERVICE.updateUserInfo(signUpDto);
