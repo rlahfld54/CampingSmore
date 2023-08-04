69b7efcea03410421940446a724717b11bf1ff0f
JeongHoBBang <dlawjdghbn@naver.com>
2023-08-04 15:57:58 +0900

    댓글 수정-임정호

--- src/test/java/com/green/campingsmore/community/comment/CommentMapperTest.java
+++ src/test/java/com/green/campingsmore/community/comment/CommentMapperTest.java
@@ -2,12 +2,16 @@ package com.green.campingsmore.community.comment;
 
 import com.green.campingsmore.community.comment.model.CommentEntity;
 import com.green.campingsmore.community.comment.model.CommentInsDto;
+import com.green.campingsmore.community.comment.model.CommentPageDto;
+import com.green.campingsmore.community.comment.model.CommentVo;
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 @MybatisTest
@@ -34,6 +38,17 @@ class CommentMapperTest {
     }
     @Test
     void updComment() {
+        CommentEntity entity = new CommentEntity();
+        entity.setIcomment(1L);
+        entity.setIuser(1L);
+        entity.setCtnt("수정됨");
+
+
+        Long result = mapper.updComment(entity);
+
+        assertEquals(1,result);
+        assertEquals(1,entity.getIcomment());
+        assertEquals(1,entity.getIuser());
     }
 
     @Test
@@ -42,6 +57,15 @@ class CommentMapperTest {
 
     @Test
     void selComment() {
+        CommentPageDto dto = new CommentPageDto();
+        List<CommentVo> list = mapper.selComment(dto);
+        assertEquals(6,list.size());
+
+        CommentVo item1 = list.get(0);
+        assertEquals(1,item1.getIboard());
+
+        assertNotNull(item1.getCtnt());
+        assertEquals("hello",item1.getCtnt());
     }
 
     @Test
