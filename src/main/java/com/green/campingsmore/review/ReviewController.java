bf9506075d1f84fe69059b809a97ca163b312c11
장하은 <haun0@naver.com>
2023-08-04 18:20:42 +0900

    item 추가, 삭제 - haeun

--- src/main/java/com/green/campingsmore/review/ReviewController.java
+++ src/main/java/com/green/campingsmore/review/ReviewController.java
@@ -4,6 +4,7 @@ import com.green.campingsmore.review.model.ReviewInsDto;
 import com.green.campingsmore.review.model.ReviewPageDto;
 import com.green.campingsmore.review.model.ReviewRes;
 import com.green.campingsmore.review.model.ReviewSelVo;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
@@ -18,12 +19,23 @@ import java.util.List;
 public class ReviewController {
     private final ReviewService SERVICE;
 
-    @PostMapping("/write")
+/*    @PostMapping("/write")
+    @Operation(summary = "리뷰 추가"
+            , description = "" +
+            "\"iuser\": [-] 유저 PK,<br>" +
+            "\"iorder\": [-]  아이템 썸네일 pic url,<br>" +
+            "\"iitem\": [-] 아이템 PK,<br>" +
+            "\"reviewCtnt\": [-] 리뷰 내용,<br>" +
+            "\"starRating\": [-] 별점,<br>" +
+            "\"pic\": [-] 사진 이미지<br>")
     public int postReview(@RequestBody ReviewInsDto dto) {
         return SERVICE.insReview(dto);
-    }
+    }*/
 
     @GetMapping("/{iitem}/detail")
+    @Operation(summary = "리뷰 추가"
+            , description = "" +
+            "\"iitem\": [-] 아이템 PK<br>" )
     public ReviewRes getReview(@PathVariable Long iitem,
                                @RequestParam(defaultValue = "1")int page,
                                @RequestParam(defaultValue = "5") int row) {
