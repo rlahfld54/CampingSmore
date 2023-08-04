1add8950dab181057e0c395d51a98612d9ef4c47
JeongHoBBang <dlawjdghbn@naver.com>
2023-08-04 11:46:54 +0900

    댓글 수정-임정호

--- src/test/java/com/green/campingsmore/community/comment/CommentMapperTest.java
+++ src/test/java/com/green/campingsmore/community/comment/CommentMapperTest.java
@@ -1,50 +1,50 @@
-////package com.green.campingsmore.community.comment;
-////
-////import com.green.campingsmore.community.comment.model.CommentEntity;
-////import com.green.campingsmore.community.comment.model.CommentInsDto;
-////import lombok.RequiredArgsConstructor;
-////import org.junit.jupiter.api.Test;
-////import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
-////import org.springframework.beans.factory.annotation.Autowired;
-////import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-////import org.springframework.test.context.ActiveProfiles;
-////
-////import static org.junit.jupiter.api.Assertions.*;
-////@MybatisTest
-////@ActiveProfiles("test")
-////@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-////class CommentMapperTest {
-////    @Autowired
-////    private CommentMapper mapper;
-////
-////
-////    @Test
-////    void insComment() {
-////        CommentEntity entity = new CommentEntity();
-////        entity.setIboard(7L);
-////        entity.setIuser(1L);
-////        entity.setCtnt("테스트1");
-////
-////
-////
-////        Long result = mapper.insComment(entity);
-////        assertEquals(1,result);
-////        assertEquals(6,entity.getIboard());
-////        assertEquals(1,entity.getIuser());
-////    }
-//    @Test
-//    void updComment() {
-//    }
-//
-//    @Test
-//    void delComment() {
-//    }
-//
-//    @Test
-//    void selComment() {
-//    }
-//
-//    @Test
-//    void maxComment() {
-//    }
-//}
+package com.green.campingsmore.community.comment;
+
+import com.green.campingsmore.community.comment.model.CommentEntity;
+import com.green.campingsmore.community.comment.model.CommentInsDto;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+@MybatisTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class CommentMapperTest {
+    @Autowired
+    private CommentMapper mapper;
+
+
+    @Test
+    void insComment() {
+        CommentEntity entity = new CommentEntity();
+        entity.setIboard(9L);
+        entity.setIuser(1L);
+        entity.setCtnt("테스트1");
+
+
+
+        Long result = mapper.insComment(entity);
+        assertEquals(1,result);
+        assertEquals(9,entity.getIboard());
+        assertEquals(1,entity.getIuser());
+    }
+    @Test
+    void updComment() {
+    }
+
+    @Test
+    void delComment() {
+    }
+
+    @Test
+    void selComment() {
+    }
+
+    @Test
+    void maxComment() {
+    }
+}
