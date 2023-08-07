6b29ebdc40711739fa93ab218d165094e50b5645
황주은 <rlahfld54@naver.com>
2023-08-07 13:06:06 +0900

    관리자 유저 관리 페이지 - 황주은

--- src/main/java/com/green/campingsmore/admin/user/AdminUserController.java
+++ src/main/java/com/green/campingsmore/admin/user/AdminUserController.java
@@ -1,64 +1,51 @@
-//package com.green.campingsmore.admin.user;
-//
-//import com.green.campingsmore.CommonRes;
-//import com.green.campingsmore.sign.model.SignInResultDto;
-//import io.swagger.v3.oas.annotations.Operation;
-//import io.swagger.v3.oas.annotations.tags.Tag;
-//import jakarta.servlet.http.HttpServletRequest;
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.http.HttpHeaders;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseCookie;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@Slf4j
-//@RequiredArgsConstructor
-//@RestController
-//@Tag(name = "관리자 유저 관리 페이지")
-//@RequestMapping("/admin")
-//public class AdminUserController {
-//
-//    @PostMapping("/sign-in")
-//    @Operation(summary = "로그인",
-//            description = "Try it out -> Execute 눌러주세요 \n\n " +
-//                    "id:  아이디 \n\n " +
-//                    "password : 비밀번호 \n\n "
-//    )
-//    public SignInResultDto signIn(HttpServletRequest req, @RequestParam String id, @RequestParam String password){
-//
-//        String ip = req.getRemoteAddr();
-//        log.info("[signIn] 로그인을 시도하고 있습니다. id: {}, pw: {}, ip: {}", id, password, ip);
-//
-//        SignInResultDto dto = SERVICE.signIn(id, password, ip);
-//        if (dto.getCode() == CommonRes.SUCCESS.getCode()) {
-//            log.info("[signIn] 정상적으로 로그인 되었습니다. id: {}, token: {}", id, dto.getAccessToken());
-//        }
-//
-//        return dto;
-//    }
-//
-//    @PostMapping("/logout")
-//    @Operation(summary = "로그아웃",
-//            description = "Try it out -> Execute 눌러주세요 \n\n "
-//    )
-//    public ResponseEntity<?> logout(HttpServletRequest req) {
-//        SERVICE.logout(req);
-//        ResponseCookie responseCookie = ResponseCookie.from("refresh-token", "")
-//                .maxAge(0)
-//                .path("/")
-//                .build();
-//
-//        log.info("// ResponseCookie :{}",responseCookie);
-//        log.info("// 로그아웃 완료!!!");
-//
-//        return ResponseEntity
-//                .status(HttpStatus.OK)
-//                .header(HttpHeaders.SET_COOKIE, responseCookie.toString())
-//                .build();
-//    }
-//}
+package com.green.campingsmore.admin.user;
+
+import com.green.campingsmore.CommonRes;
+import com.green.campingsmore.sign.SignService;
+import com.green.campingsmore.sign.model.SignInResultDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@Tag(name = "관리자 유저 관리 페이지")
+@RequestMapping("/admin")
+public class AdminUserController {
+    // 관리자 로그아웃은 유저 로그아웃과 동일한 것을 쓴다.
+    private final SignService signService;
+    private final AdminUserService SERVICE;
+
+    @PostMapping("/sign-in")
+    @Operation(summary = "관리자 로그인",
+            description = "Try it out -> Execute 눌러주세요 \n\n " +
+                    "id:  아이디 \n\n " +
+                    "password : 비밀번호 \n\n "
+    )
+    public SignInResultDto signIn(HttpServletRequest req, @RequestParam String id, @RequestParam String password){
+
+        String ip = req.getRemoteAddr();
+        log.info("[signIn] 관리자 로그인을 시도하고 있습니다. id: {}, pw: {}, ip: {}", id, password, ip);
+
+        SignInResultDto dto = signService.signIn(id, password, ip);
+        if (dto.getCode() == CommonRes.SUCCESS.getCode()) {
+            log.info("[signIn] 정상적으로 관리자 로그인 되었습니다. id: {}, token: {}", id, dto.getAccessToken());
+        }
+
+        return dto;
+    }
+
+    @GetMapping("/users")
+    @Operation(summary = "관리자 - 모든 유저 리스트 보기",
+            description = "Try it out -> Execute 눌러주세요 \n\n "
+    )
+    public void selectAllusers(){
+        SERVICE.selectAllusers();
+    }
+
+
+}
---
+++ src/main/java/com/green/campingsmore/sign/model/LoginDto.java
@@ -0,0 +1,9 @@
+package com.green.campingsmore.sign.model;
+
+import lombok.Data;
+
+@Data
+public class LoginDto {
+    private String uid;
+    private String password;
+}
