2d5a1dc6e93d19b483d614a6cbe439485c7cfcb3
황주은 <rlahfld54@naver.com>
2023-08-07 15:40:46 +0900

    TDD 세팅 중 - 황주은

---
+++ src/test/java/com/green/campingsmore/config/security/UserDetailsMapperTest.java
@@ -0,0 +1,40 @@
+package com.green.campingsmore.config.security;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserDetailsMapperTest {
+
+    @Test
+    void signUp() {
+    }
+
+    @Test
+    void getByUid() {
+    }
+
+    @Test
+    void updUserToken() {
+    }
+
+    @Test
+    void selUserToken() {
+    }
+
+    @Test
+    void delYnUser() {
+    }
+
+    @Test
+    void searchID() {
+    }
+
+    @Test
+    void updateUserInfo() {
+    }
+
+    @Test
+    void searchPW() {
+    }
+}
--- src/test/java/com/green/campingsmore/sign/SignControllerTest.java
+++ src/test/java/com/green/campingsmore/sign/SignControllerTest.java
@@ -1,11 +1,34 @@
 package com.green.campingsmore.sign;
 
+import com.green.campingsmore.MockMvcConfig;
+import com.green.campingsmore.order.cart.CartController;
+import com.green.campingsmore.order.cart.CartService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+@MockMvcConfig
+@WebMvcTest(SignController.class)
+@AutoConfigureMockMvc(addFilters = false)
 class SignControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private SignService service;
 
     @Test
     void signIn() {
+    }
+
+    @Test
+    void logout() {
     }
 
     @Test
