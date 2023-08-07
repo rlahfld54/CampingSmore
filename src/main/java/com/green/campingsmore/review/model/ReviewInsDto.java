513406b89f1f89354947e4f123316af03c2b8142
장하은 <haun0@naver.com>
2023-08-07 15:37:09 +0900

    리뷰 수정중 - haeun

--- src/main/java/com/green/campingsmore/review/model/ReviewInsDto.java
+++ src/main/java/com/green/campingsmore/review/model/ReviewInsDto.java
@@ -1,15 +1,16 @@
 package com.green.campingsmore.review.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
 
 @Data
+@AllArgsConstructor
 public class ReviewInsDto {
     private Long iuser;
     private Long iorder;
     private Long iitem;
     private String reviewCtnt;
     private int starRating;
-    private String pic;
 }
