77ad61a3bb2f99233f574f019d08cd6f99f42f3c
황주은 <rlahfld54@naver.com>
2023-08-11 17:57:01 +0900

    시큐리티 수정 - 황주은

--- src/main/java/com/green/campingsmore/config/security/SecurityConfiguration.java
+++ src/main/java/com/green/campingsmore/config/security/SecurityConfiguration.java
@@ -7,8 +7,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
