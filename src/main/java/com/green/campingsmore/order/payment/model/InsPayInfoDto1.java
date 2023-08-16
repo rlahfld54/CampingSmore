f463bc99c0118fc42374be17f116ea3989fca7c5
신형주 <gbz3267@gmail.com>
2023-08-16 10:48:49 +0900

    payserivce 수정 - 신형주

--- src/main/java/com/green/campingsmore/order/payment/model/InsPayInfoDto1.java
+++ src/main/java/com/green/campingsmore/order/payment/model/InsPayInfoDto1.java
@@ -1,10 +1,13 @@
 package com.green.campingsmore.order.payment.model;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
-@Data
+@Getter
+@Setter
 public class InsPayInfoDto1 extends InsPayInfoDto {
     private Long iuser;
 }
